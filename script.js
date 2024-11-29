b8dc8705f0992d524a16eee90bb8c6910724f4c9
Matvke <matvei31055@gmail.com>
2024-11-29 21:37:28 +0500

    Delete button

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ document.getElementById('weather-form').addEventListener('submit', e => {
     e.preventDefault();
     const latitude = parseFloat(document.getElementById('latitude').value);
     const longitude = parseFloat(document.getElementById('longitude').value);
-  
+
     if (isNaN(latitude) || isNaN(longitude)) {
         alert('Введите корректные значения широты и долготы');
     } else {
         weatherWidget.run(latitude, longitude);
         weatherWidget1.run(latitude, longitude);        
     }
-  });
+});
 
 // виджеты
 const weatherWidget = new Application('widget-container');
 const weatherWidget1 = new Application('widget-container1');
 
+window.removeWidget = function(containerId) {
+    const container = document.getElementById(containerId);
+    if (container) {
+        container.innerHTML = '';
+    }
+}
